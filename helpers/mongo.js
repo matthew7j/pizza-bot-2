7b02ef08838c70f2bf1726e78f91c5a73ae4aaab
Matt <mjones@accusoft.com>
2021-04-23 21:10:21 -0400

    Big code cleanup

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -6,17 +6,57 @@ require('dotenv').config();
 const client = new MongoClient(process.env.MONGO_URI, { useUnifiedTopology: true });
 
 const mongo = async options => {
-    let collection;
-    try {
-        await client.connect();
-        const db = await client.db(options.db);
-        collection = await db.collection(options.collection);
-    } catch (err) {
-        console.error(err);
-    }
-    return collection;
+  let collection;
+  try {
+    await client.connect();
+    const db = await client.db(options.db);
+    collection = await db.collection(options.collection);
+  } catch (err) {
+    console.error(err);
+  }
+  return collection;
 };
 
+const givePizzaOp = (userId, pizzas) => {
+  return {
+    updateOne: {
+      filter: {
+        userId
+      },
+      update: {
+        $inc: {
+          pizzas
+        }
+      }
+    }
+  };
+};
+
+const takePizzaOp = (userId, pizzas) => {
+  return {
+    updateOne: {
+      filter: {
+        userId
+      },
+      update: {
+        $inc: {
+          pizzas: -pizzas
+        }
+      }
+    }
+  };
+};
+
+const performMongoOps = async ops => {
+  const mongoOptions = { db: 'pizza_bot', collection: 'users' };
+  const collection = await mongo(mongoOptions);
+  console.log(`ops: ${JSON.stringify(ops)}`);
+  await collection.bulkWrite(ops);
+}
+
 module.exports = {
-    mongo
+    mongo,
+    takePizzaOp,
+    givePizzaOp,
+    performMongoOps
 };
